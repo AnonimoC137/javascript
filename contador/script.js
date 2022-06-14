df5d403f63b3b4961f8e074ae5e769e70e6b1e7d
AnonimoC137 <alexandrecoimbra44@gmail.com>
2022-06-13 23:45:27 -0300

    att

--- contador/script.js
+++ contador/script.js
@@ -19,11 +19,11 @@ function contar() {
     }
     if (i < f) {
       for (let c = i;c <= f;c += p) {
-        res.innerHTML += `${c}\u{1F449}`
+        res.innerHTML += `\u{1F449}${c}`
       }   
     } else {
        for (let c = i;c >= f;c -= p) {
-          res.innerHTML += `${c}\u{1F449}`
+          res.innerHTML += `\u{1F449}${c}`
        }
       
     }
