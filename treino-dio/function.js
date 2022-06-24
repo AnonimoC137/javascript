a6cf90df96e693d2375c26948600da24f9faabd8
AnonimoC137 <alexandrecoimbra44@gmail.com>
2022-06-23 23:19:13 -0300

    att

---
+++ treino-dio/function.js
@@ -0,0 +1,5 @@
+function soma(a, b) {//aqui estamos declarando a function
+    console.log(a + b);//para mostrar na tela
+    return a + b;//para retornar o valor para usar e manipular em uma outra function no programa.
+}
+soma(3, 5);//aqui estamos "chamando" a function
