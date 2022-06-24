7e9080c44e2d1bb06797c66ff44c6461583f517f
AnonimoC137 <alexandrecoimbra44@gmail.com>
2022-06-23 22:51:56 -0300

    att

---
+++ treino-dio/variavel-constante.js
@@ -0,0 +1,6 @@
+//var preco = 2;
+var desconto = 0.2;
+//a constante deve ser escrita em MAIUSCULO, para facilitar a visualização, é uma boa pratica em javascript
+const PRECO = 2;
+
+var total = PRECO - desconto;
