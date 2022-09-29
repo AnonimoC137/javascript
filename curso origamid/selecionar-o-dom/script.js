acdef7faae5806346f791009f41a7171863fc813
AnonimoC137 <alexandrecoimbra44@gmail.com>
2022-09-29 00:26:13 -0300

    att

--- curso origamid/selecionar-o-dom/script.js
+++ curso origamid/selecionar-o-dom/script.js
@@ -2,3 +2,22 @@
 const retornaImg = document.querySelectorAll('.grid-section img');
 console.log(retornaImg);
 
+//retorne no console apenas as imagens que começaram com a palavra imagem
+
+const somenteImg = document.querySelectorAll('img[src^="../imagens/imagem"]');
+console.log(somenteImg);
+
+//selecione todos os links internos (onde o href começa com #)
+
+const linksInternos = document.querySelectorAll('[href^="#"]');
+console.log(linksInternos);
+
+//selecione o primeiro h2 dentro do .animais-descricao
+
+const mostraH2 = document.querySelector('.animais-descricao h2');
+console.log(mostraH2);
+
+//selecione o ultimo p do site 
+
+const ultimoP = document.querySelector('.copy p');
+console.log(ultimoP);
