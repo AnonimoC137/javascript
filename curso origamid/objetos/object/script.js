5beb091eb2d63528a5d0688b7c1fd28194c08577
AnonimoC137 <alexandrecoimbra44@gmail.com>
2022-11-03 23:32:35 -0300

    att

---
+++ curso origamid/objetos/object/script.js
@@ -0,0 +1,26 @@
+//crie uma funão que verifique
+//corretamente o tipo de dado
+
+
+
+
+
+
+// crie um objeto quedrado com 
+//a propriedade lados e torne
+//ele imutavel
+
+
+
+
+//previna qualquer mudança
+//no objeto abaixo
+const configuracao = {
+    width: 800,
+    height: 600,
+    backgroud: '#333'
+}
+
+//liste o nome de todos
+//as prpriedades do
+//prototipo de string e array
