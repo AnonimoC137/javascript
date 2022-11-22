5876b93d3a26e2b1b74b129c9b4d26c85cf6ac71
AnonimoC137 <alexandrecoimbra44@gmail.com>
2022-11-22 20:48:41 -0300

    att

---
+++ curso origamid/efeitos-no-dom/dropdown-menu/js/script.js
@@ -0,0 +1,19 @@
+import initScrollSuave from './modules/scroll-suave.js'
+import initAnimacaoScroll from './modules/scroll-animacao.js'
+import initAccordion from './modules/accordion.js'
+import initTabNav from './modules/tab-nav.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+
+
+
+
+
