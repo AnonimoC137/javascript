e3e43746b2559d28af62f69b2793dcaca4f2c0fe
AnonimoC137 <alexandrecoimbra44@gmail.com>
2022-11-22 18:49:20 -0300

    concluindo o modal

--- curso origamid/efeitos-no-dom/modal/js/script.js
+++ curso origamid/efeitos-no-dom/modal/js/script.js
@@ -2,12 +2,14 @@ import initScrollSuave from './modules/scroll-suave.js'
 import initAnimacaoScroll from './modules/scroll-animacao.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tab-nav.js'
+import initModal from './modules/modal.js'
 
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal()
 
 
 
