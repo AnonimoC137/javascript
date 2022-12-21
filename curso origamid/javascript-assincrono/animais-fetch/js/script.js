aadf80f68c82f298ddb6eacd2668b2e6e1082fbf
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2022-12-20 23:34:11 -0300

    finalizado o animais fetch

--- curso origamid/javascript-assincrono/animais-fetch/js/script.js
+++ curso origamid/javascript-assincrono/animais-fetch/js/script.js
@@ -6,7 +6,6 @@ import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
 import initDropdownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
-import initAnimaNumeros from './modules/anima-numeros.js'
 import initfuncionamento from './modules/funcionamento.js'
 import initFetchAnimais from './modules/animais-fetch.js'
 
@@ -22,7 +21,6 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initfuncionamento();
 initFetchAnimais();
 
