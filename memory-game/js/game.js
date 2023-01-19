72cb50c9009dfe43ed5d2d0d9ca397d8e11a3810
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-01-18 22:30:00 -0300

    att

--- memory-game/js/game.js
+++ memory-game/js/game.js
@@ -1,15 +1,59 @@
 const grid = document.querySelector('.grid');
 
-const createCard = () => {
-    const card = document.createElement('div');
-    const front = document.createElement('div');
-    const back = document.createElement('div');
+const characters = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'summer',
+    'meeseeks',
+    'scroopy',
+    'rick',
+];
 
-    card.className = 'card';
-    front.className = 'face front';
-    back.className = 'face back';
+
+//funcao que facilita a criacao de tag e class
+//dos cards
+const createElement = (tag, className) => {
+    const element = document.createElement(tag)
+    element.className = className;
+    return element;
+}
+
+
+//funcao que cria os cards
+//dentro dela tem o codigo para as imagens aleatorias
+const createCard = (character) => {
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    front.style.backgroundImage = `url('../imagens/${character}.png')`;
 
     card.appendChild(front);
     card.appendChild(back);
 
+    return card;
+
 }
+
+//funcao que carrega o jogo 
+//ela tbm add o card ao grid
+//coloca cada personagem do array da funcao
+//de criar card
+//foi criado um array duplicado do original
+//contendo dois do mesmo para os cards duplicarem
+const loadGame = () => {
+    const arrayDuplicado = [...characters, ...characters];
+    const misturaArray = arrayDuplicado.sort(() => Math.random() - 0.5);
+    misturaArray.forEach((character) => {
+        const card = createCard(character);
+        grid.appendChild(card)
+    });
+}
+
+loadGame()
+
+
