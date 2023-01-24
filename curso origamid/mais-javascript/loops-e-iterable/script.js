72f493e79ba61baa94d0414ed55ab75bed9b8ba1
Alexandre Coimbra nunes <99856025+AnonimoC137@users.noreply.github.com>
2023-01-24 01:30:28 -0300

    att

---
+++ curso origamid/mais-javascript/loops-e-iterable/script.js
@@ -0,0 +1,23 @@
+//Crie 4 li's na pagina
+//Utilizando o for...of
+//adicione uma classe a cada li
+const li = document.querySelectorAll('li');
+
+ 
+
+for(const novoLi of li) {
+    novoLi.classList.add('nova-classe')
+    console.log(novoLi)
+ }
+
+//Utilize o for...in para listar 
+//todos as propriedades e valores
+//do objeto window
+const array =  []
+
+for(const newWindow in window) {
+    
+}
+
+ 
+
